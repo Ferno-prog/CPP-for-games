e2e40cfd95f5461855fec9ed3a44eb82896d77b4
Ferno-prog <arandomemail751@gmail.com>
2026-06-01 18:03:59 +0200

    Fixed version of "Type Conversion"

---
+++ Type Conversion/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    double d = 42.99;
+    float f = static_cast<float>(d);
+    int i = static_cast<int>(f);
+    char c = static_cast<char>(i);
+    bool b = static_cast<bool>(c);
+
+    std::cout << d << '\n'
+        << f << '\n'
+        << i << '\n'
+        << c << '\n'
+        << b << '\n';
+
+    return 0;
+}
